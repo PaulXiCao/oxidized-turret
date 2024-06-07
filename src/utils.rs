7f2bbdf6a72609c77f6111dd2d1a9d230e35f460
Alexander Zibert <3660540+alexander-zibert@users.noreply.github.com>
2024-06-07 21:55:26 +0200

    add JS log binding for debugging

--- src/utils.rs
+++ src/utils.rs
@@ -75,3 +75,9 @@ pub fn set_panic_hook() {
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
 }
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(s: &str);
+}
